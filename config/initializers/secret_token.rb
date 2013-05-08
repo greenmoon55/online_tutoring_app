aa93caedb120047bd080b9b8102269cdf16602af
greenmoon55 <greenmoon55@gmail.com>
2013-05-08 14:45:29 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineTutoringApp::Application.config.secret_token = '5b049d23bc296c81863da93951e96e560495f3dc8ccd4be65ae790a439fe741ac987d3de0997097061a93dd81027952f1258cdfaf7daec8a2dd10f7bc2f02d5d'
