8598c63e5d441e91c7c941c32943f9a7b4edad84
greenmoon55 <greenmoon55@gmail.com>
2013-05-10 21:20:36 +0800

    修改注册页面，添加Subjects表，修改README.md

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ online_tutoring_app
 -------
 
 所有 HTML/CSS/JavaScript、Ruby 代码均采用两个空格缩进，不用tab...
+
+配置说明
+-------
+
+> bundle install --without production
+
+> bundle exec rake:db migrate
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130510081123) do
+ActiveRecord::Schema.define(:version => 20130510125817) do
 
   create_table "districts", :force => true do |t|
+    t.string "name", :null => false
+  end
+
+  create_table "subjects", :force => true do |t|
     t.string "name", :null => false
   end
 
