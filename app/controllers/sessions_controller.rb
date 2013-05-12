c8d43937443be243c48aa203cfd090421a81b07b
greenmoon55 <greenmoon55@gmail.com>
2013-05-12 15:13:42 +0800

    修改登录界面

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email_and_role(params[:email].downcase, params[:type] == 'student')
+    role = (params[:role] == "student")
+    user = User.find_by_email_and_role(params[:email].downcase, role)
     if user && user.authenticate(params[:password])
 #      if params[:remember_me]
 #        cookies.permanent[:remember_token] = user.remember_token
 #      else
         session[:user_id] = user.id
+        session[:role] = role
 #      end
       self.current_user = user
-      logger.info user
+      #logger.info user
       redirect_to root_url
     else
       flash.now[:error] = "Email或密码错误"
