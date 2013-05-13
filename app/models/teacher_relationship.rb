fee1a7f54bf3ee10c06748e44bc926865c38bb0e
greenmoon55 <greenmoon55@gmail.com>
2013-05-13 20:07:25 +0800

    貌似可以多选科目了。。太不容易了。。

--- app/models/teacher_relationship.rb
+++ app/models/teacher_relationship.rb
@@ -1,3 +1,9 @@
 class TeacherRelationship < ActiveRecord::Base
   attr_accessible :subject_id, :user_id
+
+  belongs_to :subject
+  belongs_to :user
+
+  validates :subject_id, :user_id, presence: :true
+  validates :user_id, :uniqueness => { scope: :subject_id }
 end
