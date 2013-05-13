703a916c5d01afa247c535234a6eb3f63085bf99
greenmoon55 <greenmoon55@gmail.com>
2013-05-13 21:21:47 +0800

    注册成功提示，修改email验证的bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: {case_sensitive: false, scope: :role}
+                    uniqueness: {case_sensitive: false}
 
   validates :password, presence: true, length: { minimum: 7 },
             if: :should_validate_password?
