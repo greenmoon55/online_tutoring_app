ae456fbda2e7c0e8a48ea85bd6ec9b8e1badcef7
greenmoon55 <greenmoon55@gmail.com>
2013-05-16 16:10:40 +0800

    创建messages表

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130516062112) do
+ActiveRecord::Schema.define(:version => 20130516075405) do
 
   create_table "degrees", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string "name"
   end
 
   create_table "districts", :force => true do |t|
     t.string "name", :null => false
+  end
+
+  create_table "messages", :force => true do |t|
+    t.integer  "sender_id",   :null => false
+    t.integer  "receiver_id", :null => false
+    t.string   "content"
+    t.datetime "created_at",  :null => false
   end
 
   create_table "relationships", :force => true do |t|
