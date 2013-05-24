a6935f360ccfc15c8bb257dc90fef897cdc89c02
greenmoon55 <greenmoon55@gmail.com>
2013-05-24 16:26:58 +0800

    点击聊天按钮即可添加当前用户到聊天窗

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -8,5 +8,29 @@ $(document).ready(function() {
   })
   $("#chat-with-button").click(function() {
     $("#chat-box").show();
+    var path = window.location.pathname;
+    var uid = path.match(/\/users\/(\d*)/)[1];
+    var username = $("#username").text();
+    var isOnline = $("#img_online").length;
+    addUser(uid, username, isOnline);
   });
 });
+
+function addUser(uid, username, isOnline) {
+  console.log("addUser " + uid + username + isOnline);
+  var statusSpan = document.createElement("span");
+  $(statusSpan).attr("class", "chat-status");
+  if (isOnline) {
+    $(statusSpan).addClass("chat-online");
+  } else {
+    $(statusSpan).addClass("chat-offline");
+  }
+  var nameSpan = document.createElement("span");
+  $(nameSpan).attr("class", "chat-username");
+  $(nameSpan).append(username);
+  var li = document.createElement("li");
+  $(li).append(statusSpan);
+  $(li).append(nameSpan);
+  $(li).attr("title", username);
+  $("#chat-left ul").append(li);
+}
