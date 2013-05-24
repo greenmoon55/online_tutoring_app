4aac3a1611d9ba510fcfbd1ee7da864b8a62270d
greenmoon55 <greenmoon55@gmail.com>
2013-05-24 22:06:14 +0800

    可以从聊天窗口发消息啦!

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
   $("#chat-close").click(function() {
     $("#chat-box").hide();
     return false;
-  })
+  });
   $("#chat-left").on("click", "li", function() {
     activateItem(this);
-  })
+  });
   $("#chat-with-button").click(function() {
     $("#chat-box").show();
     var path = window.location.pathname;
@@ -17,29 +17,50 @@ $(document).ready(function() {
     var isOnline = $("#img_online").length;
     addUser(uid, username, isOnline);
   });
+  $("#chat-sendbutton").click(function() {
+    alert("sendbutton");
+    $("#new_message").submit();
+    return false;
+  });
+  $("#new_message").on("submit", function() {
+    console.log($(this).serialize());
+    $.ajax({
+      url: $(this).prop("action"),
+      type: "POST",
+      data: $(this).serialize()
+    })
+    return false;
+  });
 });
 
 function addUser(uid, username, isOnline) {
   console.log("addUser " + uid + username + isOnline);
-  var statusSpan = document.createElement("span");
-  $(statusSpan).attr("class", "chat-status");
-  if (isOnline) {
-    $(statusSpan).addClass("chat-online");
-  } else {
-    $(statusSpan).addClass("chat-offline");
+  str = "#chat-left li#" + uid;
+  // 判断是否已存在该用户
+  if (!$(str).length) {
+    var statusSpan = document.createElement("span");
+    $(statusSpan).attr("class", "chat-status");
+    if (isOnline) {
+      $(statusSpan).addClass("chat-online");
+    } else {
+      $(statusSpan).addClass("chat-offline");
+    }
+    var nameSpan = document.createElement("span");
+    $(nameSpan).attr("class", "chat-username");
+    $(nameSpan).append(username);
+    var li = document.createElement("li");
+    $(li).append(statusSpan);
+    $(li).append(nameSpan);
+    $(li).attr("title", username);
+    $(li).attr("id", uid);
+    $("#chat-left ul").append(li);
   }
-  var nameSpan = document.createElement("span");
-  $(nameSpan).attr("class", "chat-username");
-  $(nameSpan).append(username);
-  var li = document.createElement("li");
-  $(li).append(statusSpan);
-  $(li).append(nameSpan);
-  $(li).attr("title", username);
-  $("#chat-left ul").append(li);
+  activateItem(str);
 }
 
 // 激活当前点击的对象
 function activateItem(item) {
   $("#chat-left li").removeClass("chat-active");
   $(item).addClass("chat-active");
+  $("#message_receiver_id").val($(item).attr("id"));
 }
