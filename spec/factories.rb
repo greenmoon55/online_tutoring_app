6d334c7d7df216ebc7efe68376f65f0b67cd486c
yeziyll <yeziyll@qq.com>
2013-06-03 15:29:47 +0800

    写了一些页面测试，但是未通过啊，是我写的问题还是你们写的问题

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,6 @@
+# -*- encoding : utf-8 -*-
 FactoryGirl.define do
 
-	factory :user do
-		name "fjkdrwerwr"
-		password "hdfjkshfk"
-		password_confirmation "hdfjkshfk"
-		email "hfjdskfh@qq.com"
-		role 1
-	end
+	
 end
 		
