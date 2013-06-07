bd2d752cb96a6c35de51e3e80a41097d2574dbcf
greenmoon55 <greenmoon55@gmail.com>
2013-06-07 22:45:56 +0800

    添加清空功能，还没写这个功能的同步，捉急啊！

--- app/assets/javascripts/groupchat.js
+++ app/assets/javascripts/groupchat.js
@@ -1,9 +1,18 @@
-function onGroupChatMessage(message, roomID) {
-  console.log("test");
-  if (roomID == $("#message_room_id").val()) {
-    $("#chatroom-dialogue-list").append(message.content);
-  }
-}
+// http://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing
+CanvasRenderingContext2D.prototype.clear = 
+  CanvasRenderingContext2D.prototype.clear || function (preserveTransform) {
+    if (preserveTransform) {
+      this.save();
+      this.setTransform(1, 0, 0, 1, 0, 0);
+    }
+
+    this.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    if (preserveTransform) {
+      this.restore();
+    }           
+};
+
 var context;
 
 $(document).ready(function() {
@@ -76,3 +85,14 @@ function draw(data) {
   }
 }
 
+function clearAndUpdate() {
+  context.clear();  
+  // update here 
+}
+
+function onGroupChatMessage(message, roomID) {
+  console.log("test");
+  if (roomID == $("#message_room_id").val()) {
+    $("#chatroom-dialogue-list").append(message.content);
+  }
+}
