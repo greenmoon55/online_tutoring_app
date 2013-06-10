139e76dd0ad8b37c607d2cc359f2bdcf7307c15a
greenmoon55 <greenmoon55@gmail.com>
2013-06-10 14:42:36 +0800

    数据库又不同步，不知到是什么问题

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@
 class Room < ActiveRecord::Base
   attr_accessible :outline, :user_id
   belongs_to :user
-  has_many :room_student_relationships,dependent: :destroy
+  has_many :room_student_relationships, dependent: :destroy
   has_many :students, through: :room_student_relationships
 
   validates :user_id, presence: true
