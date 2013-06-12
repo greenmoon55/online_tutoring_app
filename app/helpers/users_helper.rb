2c81bc21f4cf9463b82542a9f20f2e165d794b42
greenmoon55 <greenmoon55@gmail.com>
2013-06-12 11:02:43 +0800

    整合搜索页面中，添加user partial，修改controller的一些注释

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,10 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
     image_tag(gravatar_url, alt: user.name, class: "avatar img-rounded")
   end
+
+  def gender(user)
+    return "女" if user.gender == 0
+    return "男" if user.gender == 1
+    return "未知"
+  end
 end
