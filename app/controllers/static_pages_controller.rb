8a233d9f09ebdcab67a0ee77881d4da6cbb981e6
greenmoon55 <greenmoon55@gmail.com>
2013-06-18 23:18:23 +0800

    热门讲师测试中

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     @annoncements = Annoncement.order("created_at DESC").limit(3)
     @advertisements = Advertisement.order("created_at DESC").limit(2)
+    @top_teachers = User.find_by_sql("SELECT * FROM users JOIN comments ON users.id = comments.teacher_id group by users.id order by count(*) DESC")
   end
 
   def about
